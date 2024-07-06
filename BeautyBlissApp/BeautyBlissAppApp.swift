153ea243571ed93c8fd5ffbec43a381ff1bcc70b
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-06 20:48:37 +0300

    Initial Commit

---
+++ BeautyBlissApp/BeautyBlissAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyBlissAppApp.swift
+//  BeautyBlissApp
+//
+//  Created by Sevde Aydın on 6.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BeautyBlissAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
