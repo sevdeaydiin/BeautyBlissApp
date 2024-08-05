0c5e45bbfc911b58fb3087cf7b05c3f4ebe56316
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-08-05 18:03:51 +0300

    Added unit test for login func

--- BeautyBlissAppTests/BeautyBlissAppTests.swift
+++ BeautyBlissAppTests/BeautyBlissAppTests.swift
@@ -9,15 +9,32 @@ import XCTest
 @testable import BeautyBlissApp
 
 final class BeautyBlissAppTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    func testLoginSuccess() throws {
+        let expectation = self.expectation(description: "Login succeeds")
+        
+        AuthServices.login(email: "test@example.com", password: "password123") { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data, "Data should not be nil on success login")
+                expectation.fulfill()
+                
+            case.failure(let error):
+                XCTFail("Login should succeed, but failed with error \(error)")
+            }
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +42,12 @@ final class BeautyBlissAppTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
