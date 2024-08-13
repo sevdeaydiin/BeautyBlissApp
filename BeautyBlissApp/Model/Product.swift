f9bc87560100770f2b5ae749be240c4090e00a55
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-08-13 18:47:35 +0300

    Added product model

--- BeautyBlissApp/Model/Product.swift
+++ BeautyBlissApp/Model/Product.swift
@@ -5,4 +5,23 @@
 //  Created by Sevde Aydın on 13.08.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ImageData: Decodable {
+    let type: String
+    let data: [UInt8]
+}
+
+struct Product: Identifiable, Decodable {
+    let _id: String
+    var id: String {
+        return _id
+    }
+    let brand: String
+    var name: String
+    var salary: Int
+    var image: ImageData
+    var productInfo: String?
+    let category: String
+    var stock: Bool
+}
