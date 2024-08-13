d6f1aa9495e247baedadd82d07011b10bc7bb89f
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-08-13 18:47:17 +0300

    New extensions

--- BeautyBlissApp/Extensions/ImagePicker.swift
+++ BeautyBlissApp/Extensions/ImagePicker.swift
@@ -5,4 +5,40 @@
 //  Created by Sevde Aydın on 13.08.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+
+    @Environment(\.dismiss) var dismiss
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            
+            self.parent.image = image
+            self.parent.dismiss()
+        }
+    }
+}
--- BeautyBlissApp/Extensions/Sizes.swift
+++ BeautyBlissApp/Extensions/Sizes.swift
@@ -5,4 +5,10 @@
 //  Created by Sevde Aydın on 13.08.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Sizes {
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+    
+}
