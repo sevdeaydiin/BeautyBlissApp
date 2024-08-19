28460a226793cb56d1b772b409d94d6301f9c9d7
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-08-20 02:14:12 +0300

    New localize keys

--- BeautyBlissApp/Locale/LocaleKeys.swift
+++ BeautyBlissApp/Locale/LocaleKeys.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct LocaleKeys {
-    enum Login: String {
+    enum Onboarding: String {
         case login = "login"
         case welcome = "welcomeBack"
         case member = "notAMember"
         case join = "joinUs"
         case email = "email"
         case password = "password"
+    }
+    
+    enum CustomTabView: String {
+        case home = "home"
+    }
+    
+    enum Profile: String {
+        case myOrders = "myOrders"
+        case wishlist = "wishlist"
+        case myAddress = "myAddress"
+        case voucher = "voucher"
+        case myAccount = "myAccount"
+        case settings = "settings"
+        case help = "help"
+        case contactUs = "contactUs"
+        case logOut = "logOut"
     }
 }
 
